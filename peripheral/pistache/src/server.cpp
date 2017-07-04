c9eb3eff7f40f276c01ab41d6cc6dac435ab099b
Aleksi Ikkala <aleksi.ikkala@gmail.com>
2017-07-04 11:09:30 +0100

    First commit

---
+++ peripheral/pistache/src/server.cpp
@@ -0,0 +1,11 @@
+#include "HelloHandler.h"
+
+int main() {
+  Net::Address addr(Net::Ipv4::any(), Net::Port(9080));
+
+  auto opts = Http::Endpoint::options().threads(1);
+  Http::Endpoint server(addr);
+  server.init(opts);
+  server.setHandler(std::make_shared<HelloHandler>());
+  server.serve();
+}
